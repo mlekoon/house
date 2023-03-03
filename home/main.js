64a09c59707e41804e137ede375ef1cf9bff9345
moumen26 <moumenkhaldi26@gmail.com>
2023-03-03 22:08:31 +0100

    firstcommit

---
+++ home/main.js
@@ -0,0 +1,7 @@
+var searchButton = document.querySelector('.search-btn i');
+var searchClass = document.querySelector('.search-btn');
+
+searchButton.addEventListener('click', () => {
+    console.log('ffrv');
+    searchClass.classList.toggle("search-active")
+})
